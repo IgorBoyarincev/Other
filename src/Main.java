6635e4666cf67371bd6acddc9dea4ddce366652b
IgorBoyarincev <boyarincev.igor@list.ru>
2022-09-18 17:46:09 +0300

    modifie Main.java

--- src/Main.java
+++ src/Main.java
@@ -46,7 +46,7 @@ public class Main {
                 }
 
                 if (isOnSale) {
-                    System.out.println("\t" + products[i] + " " + counts[i] + " шт. за " + (prices[i] * (counts[i] / 3 * 2 + counts[i] % 3)) + " руб. (распродажа!)");
+                    System.out.println("\t" + products[i] + " " + (doBonus ? counts[i] + 1 : counts[i]) + " шт. за " + (prices[i] * (counts[i] / 3 * 2 + counts[i] % 3)) + " руб. (распродажа!)");
                     sum += prices[i] * (counts[i] / 3 * 2 + counts[i] % 3);
                 } else {
                     System.out.println("\t" + products[i] + " " + (doBonus ? counts[i] + 1 : counts[i]) + " шт. за " + (prices[i] * counts[i]) + " руб");
